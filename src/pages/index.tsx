7677ab8883a534c23acb16c425cf35d0249cb90d
ManuRodgers <ManuRodgersGOAT@gmail.com>
2019-11-17 11:10:22 +1100

    delete vfile types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = props => {
-  return <div>hello world</div>;
+  return <div>hello world goat</div>;
 };
 
 export default App;
