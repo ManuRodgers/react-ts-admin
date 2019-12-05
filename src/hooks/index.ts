38faaf81dffdf3486ad0f0d3ad5a7bf85eed0bb0
ManuRodgers <ManuRodgersGOAT@gmail.com>
2019-12-05 18:14:26 +1100

    search by name and search by desc  for products finished

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,7 @@ import request, { RequestOptionsInit } from 'umi-request';
 import useSWR, { ConfigInterface, keyInterface, responseInterface } from 'swr';
 import { ICategory, IProduct } from '@/interfaces';
 import { HTTPMethod } from 'http-method-enum';
+import { SearchTypes } from '@/enums';
 export const usePrevious = <T extends {}>(value: T) => {
   const ref = useRef<T>();
   useEffect(() => {
@@ -59,6 +60,33 @@ export const useProducts = (
   const params = useMemo(() => ({ pageNum, pageSize }), [pageNum, pageSize]);
   return useRequest(
     [() => `/api/manage/product/list`, params],
+    {
+      method: HTTPMethod.GET,
+      params,
+    },
+    { revalidateOnFocus: false },
+  );
+};
+export const useSearchProducts = (
+  pageNum: number,
+  pageSize: number,
+  searchType: SearchTypes,
+  searchText: string,
+): responseInterface<
+  {
+    status: number;
+    data: { list: IProduct[]; pageNum: number; pageSize: number; pages: number; total: number };
+  },
+  Error
+> => {
+  const params = useMemo(() => ({ pageNum, pageSize, searchType, searchText }), [
+    pageNum,
+    pageSize,
+    searchType,
+    searchText,
+  ]);
+  return useRequest(
+    [() => `/api/manage/product/search`, params],
     {
       method: HTTPMethod.GET,
       params,
--- src/pages/ProductHome/ProductHome.tsx
+++ src/pages/ProductHome/ProductHome.tsx
@@ -1,29 +1,34 @@
 import React, { memo, useEffect, useState } from 'react';
 import { connect } from 'dva';
-import { Card, Table, Select, Input, Button, Icon, message } from 'antd';
+import { Card, Table, Select, Input, Button, Icon, message, Form } from 'antd';
 import { ColumnProps } from 'antd/es/table';
-
+import { FormComponentProps } from 'antd/es/form';
 import { IGlobalState, IProduct, IUmiComponent } from '@/interfaces';
 import './ProductHome.less';
 import LinkButton from '@/components/LinkButton/LinkButton';
-import { useCategories, useProducts } from '@/hooks';
+import { useCategories, useProducts, useSearchProducts } from '@/hooks';
 import { PAGE_SIZE } from '@/utils/constant';
 import { router } from 'umi';
 import { setCategories } from '@/actions/categoryActions';
 import { setProducts } from '@/actions/productActions';
+import { SearchTypes } from '@/enums';
 const mapStateToProps = ({ product }: IGlobalState) => ({
   product,
 });
 
 type ProductHomeStateProps = ReturnType<typeof mapStateToProps>;
 
-interface IProductHomeProps extends IUmiComponent, ProductHomeStateProps {}
+interface IProductHomeProps extends IUmiComponent, ProductHomeStateProps, FormComponentProps {}
 
 const ProductHome: React.FunctionComponent<IProductHomeProps> = ({
   product,
   dispatch,
   history,
 }) => {
+  const [searchText, setSearchText] = useState<string>('');
+  const [searchType, setSearchType] = useState<SearchTypes>(SearchTypes.PRODUCT_NAME);
+  console.log(`searchText`, searchText);
+  console.log(`searchType`, searchType);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [total, setTotal] = useState<number>(0);
   const [pageNum, setPageNum] = useState<number>(1);
@@ -40,11 +45,14 @@ const ProductHome: React.FunctionComponent<IProductHomeProps> = ({
   }, [categoryError, categoriesData, dispatch]);
 
   // get products from server or cache
-  const { data: productsData, isValidating: loading, error } = useProducts(pageNum, PAGE_SIZE);
+  const { data: productsData, isValidating: loading, error: productsError } = useProducts(
+    pageNum,
+    PAGE_SIZE,
+  );
   useEffect(() => {
-    if (error) {
+    if (productsError) {
       setIsLoading(loading);
-      message.error(error.message);
+      message.error(productsError.message);
     } else if (productsData && productsData.status === 0) {
       setIsLoading(loading);
       console.log(`productsData`, productsData.data);
@@ -53,7 +61,26 @@ const ProductHome: React.FunctionComponent<IProductHomeProps> = ({
       setTotal(total);
       setPageNum(pageNum);
     }
-  }, [error, productsData, loading, dispatch]);
+  }, [productsError, productsData, loading, dispatch]);
+
+  // get search products from server or cache
+  const { data: searchProductsData, error: searchProductsError } = useSearchProducts(
+    pageNum,
+    PAGE_SIZE,
+    searchType,
+    searchText,
+  );
+  useEffect(() => {
+    if (searchProductsError) {
+      message.error(searchProductsError.message);
+    } else if (searchProductsData && searchProductsData.status === 0) {
+      console.log(`searchProductSData`, searchProductsData.data);
+      const { total, list: products, pageNum } = searchProductsData.data;
+      dispatch(setProducts({ products }));
+      setTotal(total);
+      setPageNum(pageNum);
+    }
+  }, [dispatch, searchProductsData, searchProductsError]);
 
   const columns: ColumnProps<IProduct>[] = [
     {
@@ -107,14 +134,38 @@ const ProductHome: React.FunctionComponent<IProductHomeProps> = ({
       },
     },
   ];
+
   const title = (
     <span>
-      <Select value={'name'}>
-        <Select.Option value={'name'}>search by name</Select.Option>
-        <Select.Option value={`desc`}>search by description</Select.Option>
-      </Select>
-      <Input style={{ width: 150, margin: '0 15px' }} placeholder={`keyword`} />
-      <Button type={'primary'}>Search</Button>
+      <Form layout={'inline'}>
+        <Form.Item>
+          <Select
+            value={searchType}
+            onChange={(value: SearchTypes) => {
+              setSearchType(value);
+            }}
+          >
+            <Select.Option value={SearchTypes.PRODUCT_NAME}>search by name</Select.Option>
+            <Select.Option value={SearchTypes.PRODUCT_DESC}>search by description</Select.Option>
+          </Select>
+          ,
+        </Form.Item>
+        <Form.Item>
+          <Input
+            value={searchText}
+            onChange={event => {
+              setSearchText(event.target.value);
+            }}
+            style={{ width: 150, margin: '0 15px' }}
+            placeholder={`search text`}
+          />
+        </Form.Item>
+        {/*<Form.Item>*/}
+        {/*  <Button type="primary" htmlType="submit">*/}
+        {/*    Search*/}
+        {/*  </Button>*/}
+        {/*</Form.Item>*/}
+      </Form>
     </span>
   );
   const extra = (
@@ -149,4 +200,6 @@ const ProductHome: React.FunctionComponent<IProductHomeProps> = ({
   );
 };
 
-export default memo(connect(mapStateToProps)(ProductHome));
+export default memo(
+  Form.create<IProductHomeProps>({ name: 'SearchForm' })(connect(mapStateToProps)(ProductHome)),
+);
